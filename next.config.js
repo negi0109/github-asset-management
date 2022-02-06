f81d97ae6a870542ecf78608e7a901838b1c0897
negi0109 <negi0109.seg@gmail.com>
2022-02-07 02:24:11 +0900

    fix basePath

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
+const isProduction = process.env.NODE_ENV == "production"
+
 module.exports = {
   reactStrictMode: true,
-  basePath: "/github-asset-management",
+  basePath: isProduction ? "/github-asset-management" : null,
 }
