33d28f5b3fd82bfa04bb7c1ea57c6051b4856ff3
negi0109 <negi0109@users.noreply.github.com>
2022-02-08 15:19:37 +0000

    deploy: c18e7c1fbb9780155dbf78a8484ce6658d94fd18

--- _next/static/6EKCqjMRKY-HePt_YN7jY/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/895-ed2291dc919ff972.js","static/css/a826c31b904d428a.css","static/chunks/pages/index-9a6ae6b4055d842f.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
