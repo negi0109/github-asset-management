d85b0334869410633b0e8bbca522773dddf76b8c
negi0109 <negi0109@users.noreply.github.com>
2022-02-08 18:25:42 +0000

    deploy: 31181446c0777c49195f0eb7509443512007d510

--- _next/static/jnaNeyKOAT_asHbd86XRI/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/994-c9f2dbaa1de9eea1.js","static/css/a826c31b904d428a.css","static/chunks/pages/index-7a6d491d6d4baa70.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
