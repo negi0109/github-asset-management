054b29ce2addf26d4ae1391e02b00255cff82cbc
negi0109 <negi0109@users.noreply.github.com>
2022-02-11 16:30:14 +0000

    deploy: 49d3b2cb77c2c12bc45936f6c55282906818907a

--- _next/static/rGC7LTQ6hwxtvTZxYTykT/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/494-1e8dae8845ab973d.js","static/css/d7799e022ba09b15.css","static/chunks/pages/index-083ab5735db8d624.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/zIOuVySnyfUi2pOdJJXQq/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/283-627b0c38587f0baa.js","static/css/d7799e022ba09b15.css","static/chunks/pages/index-af8b7382202e8655.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
