44371e6dbe17d50fad22bc1f531c0d6e068de8c3
negi0109 <negi0109@users.noreply.github.com>
2022-02-19 19:39:34 +0000

    deploy: faf37257d8b2b3cf8c89fa351fa92160dbe3986e

--- _next/static/chunks/pages/index-a817c8af207d1fcf.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[405],{8581:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return u(6818)}])}},function(n){n.O(0,[515,687,774,888,179],(function(){return _=8581,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
